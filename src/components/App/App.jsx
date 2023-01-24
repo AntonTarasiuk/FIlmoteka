72dd4cb96f7c86f2703ee3b90c1d9edb589f2443
AntonTarasiuk <tarasiuk.anton@gmail.com>
2023-01-24 22:37:08 +0200

    Added bgc pic to header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,16 +5,7 @@ import { Footer } from "components/Footer/Footer";
 
 export const App = () => {
   return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
-    >
+    <div>
       <Header/>
       <MovieList/>
       <Footer/>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,17 +1,22 @@
 import styled from "@emotion/styled";
+import headerHomeMobile from "../../images/header-home-mobile.jpg"
+import headerHomeMobile2x from "../../images/header-home-mobile@2x.jpg"
 
 export const HeaderMain = styled.header`
     height: 230px;
     padding: 40px 15px;
     background-color: hotpink;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
     background-image: linear-gradient(to right, rgba(0, 0, 0, 0.56), rgba(0, 0, 0, 0.56)),
-      url("../../images/header-home-mobile.jpg");
+      url(${headerHomeMobile});
 
     @media (min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
     background-image: linear-gradient(to right, rgba(0, 0, 0, 0.56), rgba(0, 0, 0, 0.56)),
-        url("../../images/header-home-mobile@2x.jpg");
+        url(${headerHomeMobile2x});
     }
 `
     // display: flex;
