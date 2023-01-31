75be65910f97b37d260de25e0483e27a7fd582de
AntonTarasiuk <tarasiuk.anton@gmail.com>
2023-01-31 15:11:20 +0200

    Added sound on hover on heart svg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,25 @@
+import useSound from "use-sound";
 import { FooterComp, FoterSection, FooterText, FooterDeveloperInfo, FooterIcon, FooterLink } from "./Footer.styled"
-import sprite from "../../images/sprite.svg";    
+import sprite from "../../images/sprite.svg";
+import heartbeatSound from "../../heartbeat.ogg"
 
 export const Footer = () => {
+    const [play, { stop }] = useSound(
+        heartbeatSound,
+        { volume: 0.8 }
+    );
+
     return (
         <FooterComp id="footer-switch">
             <FoterSection>
                 <FooterText>&#169; 2022 | All Rights Reserved | &nbsp;
                     <FooterDeveloperInfo>Developed with
-                        <FooterIcon width="16" height="14">
+                        <FooterIcon
+                            width="14"
+                            height="14"
+                            onMouseEnter={() => play()}
+                            onMouseLeave={() => stop()}
+                        >
                             <use xlinkHref={`${sprite}#icon-heart`} />
                         </FooterIcon>
                         by <FooterLink href="https://github.com/AntonTarasiuk">
